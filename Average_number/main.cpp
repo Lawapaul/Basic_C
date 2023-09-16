76ee6da759ec98e174b4feb15dc66b04fedeabdc
Lawapaul <harshitshekhawat501@gmail.com>
2023-09-16 11:25:50 +0530

    New File Added

---
+++ Average_number/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int num1; 
+    int num2;
+    int num3;
+    int num4;
+    int num5;
+    
+    cout << "Enter Five Numbers with space between them: ";
+    
+    cin >> num1 >> num2 >> num3 >> num4 >> num5;
+    
+    cout <<"Average: " << (num1 + num2 + num3 + num4 + num5)/5 << endl;
+    
+    
+    
+    
+    
+    }
